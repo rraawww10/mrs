650c22fcc6f6e93ec9e45abe4d9b768daac68ee5
rraawww10 <102401151+rraawww10@users.noreply.github.com>
2023-12-02 16:14:26 +0530

    Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -21,16 +21,16 @@ class KNearestNeighbours:
 
     def fit(self):
         """Method that performs the KNN classification"""
-        # Create a list of (distance, index) tuples from the test point to each point in the data
+       
         self.distances.extend([(self.dist(self.test_point, point), i) for point, i in zip(self.data, [i for i in range(len(self.data))])])
-        # Sort the distances in ascending order
+       
         sorted_li = sorted(self.distances, key=itemgetter(0))
-        # Fetch the indices of the k nearest point from the data
+       
         self.indices.extend([index for (val, index) in sorted_li[:self.k]])
-        # Fetch the categories from the train data target
+        
         for i in self.indices:
             self.categories.append(self.target[i])
-        # Fetch the count for each category from the K nearest neighbours
+       
         self.counts.extend([(i, self.categories.count(i)) for i in set(self.categories)])
-        # Find the highest repeated category among the K nearest neighbours
+       
         self.category_assigned = sorted(self.counts, key=itemgetter(1), reverse=True)[0][0]
